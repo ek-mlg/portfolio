d40c24055441b66492d413aac6ede55e2336e858
ek-mlg <ek.mlg@icloud.com>
2023-03-16 17:33:17 +0300

    fix about

--- src/Common/Components/Main/Main.tsx
+++ src/Common/Components/Main/Main.tsx
@@ -5,16 +5,14 @@ import s from './Main.module.css';
 const Main = () => {
     return (
         <div className={s.MainBlock}>
-            <div className={s.Container}>
-                <div className={s.Text}>
-                    <h1 className={s.Title}>
-                        Kirillov <br/>
-                        <span className={s.SecondTitle}>Egor</span>
-                    </h1>
-                    <span className={s.Description}>Frontend Developer | React, Redux</span>
-                </div>
-                <div className={s.Photo}></div>
+            <div className={s.Text}>
+                <h1 className={s.Title}>
+                    Kirillov <br/>
+                    <span className={s.SecondTitle}>Egor</span>
+                </h1>
+                <span className={s.Description}>Frontend Developer | React, Redux</span>
             </div>
+            <div className={s.Photo}></div>
         </div>
     );
 };
