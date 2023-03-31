af380a9a4e52011637b3ecae6d48ab835ff55f79
ek-mlg <ek.mlg@icloud.com>
2023-03-31 16:30:20 +0300

    fix burger icon

--- src/common/components/Header/Burger/Burger.tsx
+++ src/common/components/Header/Burger/Burger.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from "react-scroll";
 import s from "./Burger.module.scss";
-import Test from "../../test/Test";
 
 const Burger = () => {
 
